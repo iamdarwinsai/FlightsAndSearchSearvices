a71b957426b4e6861833e1e2de23a28804912095
JO3Y DSVS <darushyam143@gmail.com>
2023-09-28 15:25:52 +0530

    Created a database

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express=require("express");
+const bodyParser=require("body-parser")
 
 const {PORT}=require("./config/serverConfig")
 
@@ -6,6 +7,9 @@ const {PORT}=require("./config/serverConfig")
 const startUpServer=async ()=>{
 
     const app=express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}))
 
     app.listen(PORT,()=>{
         console.log(`Started running in ${PORT}`);
